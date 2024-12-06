5f83ef54be85703c18312de23ffccc8dc14bafee
JaffMadrid <jafetmadrid4794@gmail.com>
2024-12-06 15:36:05 -0600

    Quick image fix.

--- frontend/src/components/settings/Profile.jsx
+++ frontend/src/components/settings/Profile.jsx
@@ -11,10 +11,6 @@ const Profile = () => {
   const [editEmail, setEditEmail] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [editImageUrl, setEditImageUrl] = useState("");
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const defaultImage = "https://randomuser.me/api/portraits/men/3.jpg";
-
-
 
   const getProfile = async () => {
     try {
@@ -28,8 +24,8 @@ const Profile = () => {
       setEmail(parseData.correo);
       setEditName(parseData.nombre);
       setEditEmail(parseData.correo);
-      setImageUrl(parseData.imagen_url);
-      setEditImageUrl(parseData.imagen_url);
+      setImageUrl(parseData.imagen_url  || "https://randomuser.me/api/portraits/men/3.jpg" );
+      setEditImageUrl(parseData.imagen_url || "https://randomuser.me/api/portraits/men/3.jpg");
     } catch (err) {
       console.error(err.message);
     }
@@ -75,29 +71,20 @@ const Profile = () => {
   return (
     <SettingSection icon={User} title={"Perfil"}>
       <div className="flex flex-col sm:flex-row items-center mb-6 z-20">
-        <div>
-          {/* Hide default image completely */}
-          {imageUrl && (
-            <img
-              src={imageUrl}
-              alt="Profile"
-              style={{ display: imageLoaded ? "block" : "none" }}
-              onLoad={() => setImageLoaded(true)}
-            />
-          )}
-
-          {/* Only show default while imageUrl is null/undefined */}
-          {!imageUrl && <img src={defaultImage} alt="Default Profile" />}
-        </div>
+        <img
+          src={imageUrl}
+          alt="Profile"
+          className="rounded-full w-20 h-20 object-cover mr-4"
+        />
         <div>
           <h3 className="text-lg font-semibold text-gray-100">{name}</h3>
           <p className="text-gray-400">{email}</p>
         </div>
       </div>
-      <button
+
+      <button 
         onClick={() => setShowModal(true)}
-        className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto"
-      >
+        className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200 w-full sm:w-auto">
         Editar Perfil
       </button>
 
@@ -105,10 +92,8 @@ const Profile = () => {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20">
           <div className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg rounded-xl border border-gray-700 p-6 w-96 z-20">
             <h2 className="text-xl mb-4">Editar Perfil</h2>
-
-            <label className="block mb-1 text-gray-300">
-              URL Imagen de Perfil:
-            </label>
+            
+            <label className='block mb-1 text-gray-300'>URL Imagen de Perfil:</label>   
             <input
               type="text"
               placeholder="URL de la imagen"
@@ -116,7 +101,7 @@ const Profile = () => {
               onChange={(e) => setEditImageUrl(e.target.value)}
               className="w-full px-3 py-2 bg-gray-700 text-white border-none rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-3"
             />
-            <label className="block mb-1 text-gray-300">Nombre:</label>
+            <label className='block mb-1 text-gray-300'>Nombre:</label>
             <input
               type="text"
               placeholder="Nombre"
@@ -124,7 +109,7 @@ const Profile = () => {
               onChange={(e) => setEditName(e.target.value)}
               className="w-full px-3 py-2 bg-gray-700 text-white border-none rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-3"
             />
-            <label className="block mb-1 text-gray-300">Correo:</label>
+            <label className='block mb-1 text-gray-300'>Correo:</label>
             <input
               type="email"
               placeholder="Correo"
@@ -132,7 +117,7 @@ const Profile = () => {
               onChange={(e) => setEditEmail(e.target.value)}
               className="w-full px-3 py-2 bg-gray-700 text-white border-none rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-3"
             />
-
+            
             <div className="flex justify-between mt-4">
               <button
                 onClick={() => {
@@ -141,14 +126,12 @@ const Profile = () => {
                   setEditEmail(email);
                   setEditImageUrl(imageUrl);
                 }}
-                className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-200"
-              >
+                className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-200">
                 Cancelar
               </button>
               <button
                 onClick={handleUpdateProfile}
-                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200"
-              >
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded transition duration-200">
                 Guardar
               </button>
             </div>
