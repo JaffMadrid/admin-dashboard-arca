214b27c132a2913431efdbb71e2b86980d8d9f38
JaffMadrid <jafetmadrid4794@gmail.com>
2024-12-14 03:55:48 -0600

    arreglar backend para bitacora

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -3,7 +3,7 @@ const pool = require("../db");
 const logActivity = async (userId, action, table, details) => {
   try {
     await pool.query(
-      "INSERT INTO bitacora (id_usuario, accion, tabla_afectada, detalles) VALUES ($1, $2, $3, $4)",
+      "INSERT INTO bitacora (id_auth, accion, tabla_afectada, detalles) VALUES ($1, $2, $3, $4)",
       [userId, action, table, JSON.stringify(details)]
     );
   } catch (error) {
